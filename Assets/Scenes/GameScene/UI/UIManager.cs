079cf877c254a5123d318409e4444fdee6aea3f8
MCFan99 <maclex3983@yahoo.com>
2026-02-19 11:03:31 -0800

    lex's work

--- Assets/Scenes/GameScene/UI/UIManager.cs
+++ Assets/Scenes/GameScene/UI/UIManager.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
+using UnityEngine.UIElements; // Required namespace for UI Toolkit
+using UnityEngine.SceneManagement;
+using System;
 
+<<<<<<< Updated upstream
 public class GameSceneUIManager : MonoBehaviour
 {
+=======
+public class UIManager : MonoBehaviour
+{   
+>>>>>>> Stashed changes
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
