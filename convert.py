7507f08accc7b38b8065fa7d5b55c0ee24eec269
Luke Yen <luksogami@icloud.com>
2026-02-23 12:08:55 -0800

    localhosting works with curl command

--- convert.py
+++ convert.py
@@ -1,7 +1,3 @@
-import json
-import os
-from flask import Flask, flash, request, redirect, url_for,jsonify, send_from_directory
-from werkzeug.utils import secure_filename
 import librosa
 import numpy as np
 import midiutil
@@ -9,24 +5,7 @@ import io
 import pretty_midi
 import json
 
-
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'midi', 'mid', 'wav'}
-secret_key = os.urandom(24).hex() 
-app = Flask(__name__)
-app.secret_key = secret_key
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
-DOWNLOAD_FOLDER = os.path.join(BASE_DIR, 'output')
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
-
-
-
-
-
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
+wavFile = "home/music/memphis-trap-memphis-trap-wav-349366.wav"
 
 def hertzToMidiNotes(frequency):
     if frequency <= 0:
@@ -45,10 +24,7 @@ def wavToMidi(wavFile, hopLength=512):
     )
 
     tempo, beat_frames = librosa.beat.beat_track(y=signal, sr=sampleRate)
-    if isinstance(tempo, np.ndarray):
-        tempo = float(tempo[0])
-    else:
-        tempo = float(tempo)
+    tempo = float(tempo)
     seconds_per_beat = 60.0 / tempo
 
     midi = midiutil.MIDIFile(1)
@@ -134,67 +110,9 @@ def midi_to_json(midi_bytes):
 
     return midi_dict
 
+midi_bytes = wavToMidi(wavFile)
 
+midi_json = midi_to_json(midi_bytes)
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-def audio_splicing(filename):
-    midi_bytes = wavToMidi(filename)
-
-
-    midi_json = midi_to_json(midi_bytes)
-  #  output_path = os.path.join(
-  #  app.config['DOWNLOAD_FOLDER'],
-  #  os.path.basename(filename) + "_output.json"
-#)
-
-#    with open(output_path) as f:
-#        json.dump(midi_json, f, indent = 4)
-    return jsonify(midi_json)
-@app.route('/employees', methods=['GET'])
-
-def get_employees():
-    return jsonify([{'id':1}])
-
-
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-
-
-
-            filename = secure_filename(file.filename)
-            save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-
-            file.save(save_path)  # Save first!
-
-           # audio_splicing(save_path)  # Then process
-#            save_path = (os.path.join(app.config['UPLOAD_FOLDER'],filename))
-#            file = audio_splicing(save_path)
-#            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            output_json = audio_splicing(save_path)
-            #return send_from_directory(
-            #    app.config['DOWNLOAD_FOLDER'],
-            #    os.path.basename(output_path),
-            #    as_attachment=True
-            #)
-            #return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
-            return output_json
-    return "failed"
-
-if __name__ == '__main__':
-    app.run(port=5000)
+with open("output.json", "w") as f:
+    json.dump(midi_json, f, indent = 4)
