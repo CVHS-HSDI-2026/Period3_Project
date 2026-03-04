63a63e7bd6192b8dec84fd9d0faa0bec369cb986
MCFan99 <maclex3983@yahoo.com>
2026-03-04 11:43:23 -0800

    game scene edited

--- Assets/Scenes/GameScene/UI/UIManager.cs
+++ Assets/Scenes/GameScene/UI/UIManager.cs
@@ -5,6 +5,9 @@ using System;
 
 public class GameSceneUIManager : MonoBehaviour
 {
+    Button playButton;
+    Button pauseButton;
+
     private void OnEnable()
     {
         // Get the UIDocument component
@@ -23,12 +26,42 @@ public class GameSceneUIManager : MonoBehaviour
             Debug.LogError("JumpButton not found in UXML document!");
         }
 
-        Button playButton = root.Query<Button>("PlayButton");
+        playButton = root.Query<Button>("PlayButton");
+        pauseButton = root.Query<Button>("PauseButton");
+        if (playButton != null)
+        {
+            playButton.clicked += OnPlayButtonClicked; // Register the callback method
+        }
+        else
+        {
+            Debug.LogError("PlayButton not found in UXML document!");
+        }
+        
+        if (pauseButton != null)
+        {
+            pauseButton.clicked += OnPauseButtonClicked; // Register the callback method
+        }
+        else
+        {
+            Debug.LogError("PauseButton not found in UXML document!");
+        }
     }
 
     private void OnJumpButtonClicked()
     {
         Debug.Log("Jump");
+    }
+
+    private void OnPlayButtonClicked()
+    {
+        playButton.style.visibility = Visibility.Hidden;
+        pauseButton.style.visibility = Visibility.Visible;
+    }
+
+    private void OnPauseButtonClicked()
+    {
+        pauseButton.style.visibility = Visibility.Hidden;
+        playButton.style.visibility = Visibility.Visible;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
